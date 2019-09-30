092a59d277ff720ced16bc0947fbd8a124ce9c1e
jared-gal <43042105+jared-gal@users.noreply.github.com>
2019-09-29 23:58:29 -0400

    Update wander.ino

--- HW1_4/wander.ino
+++ HW1_4/wander.ino
@@ -1,20 +1,10 @@
 /*
+Jared Gallina
+jdg272
+Biorobotics HW#1 Q4 Prey Schema
 
-Wink Robot
-Wink_BaseSketch_Rev01_01
-Version 1.0 12/2015
-
-This is a basic sketch that can be used as a starting point
-for various functionality of the Wink robot.
-
-This code was written by Kevin King and
-Dustin Soodak for Plum Geek LLC.
-Portions from other open source projects where noted.
-This code is licensed under:
-Creative Commons Attribution-ShareAlike 2.0 Generic (CC BY-SA 2.0)
-https://creativecommons.org/licenses/by-sa/2.0/
-Visit http://www.plumgeek.com for Wink information.
-Visit http://www.arduino.cc to learn about the Arduino.
+In this file the purpose is to emulate a random wandering prey that is only capable of object
+avoidance and not much else
 
 */
 
@@ -23,6 +13,24 @@ Visit http://www.arduino.cc to learn about the Arduino.
                            // machine language Wink can understand.
 
 
+#define BASE_SPEED 50
+
+#define WANDER 0
+#define TURN_AROUND 1
+#define VEER_L 2
+#define VEER_R 3
+
+//variables relevant to navigating the maze
+float rightLine;          //read value of the right inner line s\
+ensor
+float leftLine;           //read value of the left inner line se\
+nsor
+int rightMot;
+int leftMot;
+
+//variable for what state the robot is in
+int STATE;
+
 // Below is the "setup" function. It runs one time as soon as Wink turns on. You can add stuff
 // to this function if you want, but hardwareBegin() should always be the first code in the list.
 
@@ -30,6 +38,13 @@ void setup(){
   hardwareBegin();        //initialize Wink's brain to work with his circuitry
   playStartChirp();       //Play startup chirp and blink eyes
   eyesWhite(255);
+  rightMot=0;
+  leftMot=0;
+  rightLine=0;
+  leftLine=0;
+  STATE=WANDER;
+  digitalWrite(LineLeftOuter,LOW);       //turn off outer IR light sources
+  digitalWrite(LineRightOuter,LOW);
 }
 
 
@@ -37,12 +52,65 @@ void setup(){
 // inside the "loop" function will run over and over until Wink runs out of power or you turn him off.
 
 void loop(){
-  int l;
-  int r;
-  l = random(-20,20);
-  r = random(-20,20);
-  motors((30+l),(30+r));    //Both motors forward at 150 speed
-  delay(500);        //wait 1 second
+  
+  //process for reading the line sensors, this process is from the provided example code for the wink
+  //module, specifically Wink CH14 BottomSenseBasics_EX01
+  digitalWrite(LineLeftInner,LOW);
+  digitalWrite(LineRightInner,LOW);
+  delayMicroseconds(300);
+  leftLine = analogRead(LineSenseLeft);
+  rightLine = analogRead(LineSenseRight);
+  digitalWrite(LineLeftInner,HIGH);
+  digitalWrite(LineRightInner,HIGH);
+  delayMicroseconds(300);
+  leftLine = analogRead(LineSenseLeft) - leftLine;
+  rightLine= analogRead(LineSenseRight) - rightLine;
+  digitalWrite(LineLeftInner,LOW);
+  digitalWrite(LineRightInner,LOW);
+  //end of reading line sensors
+  
+ //next we set some arbitrary speeds for the robot to move in after execution
+  rightMot = BASE_SPEED + random(-10,10);
+  leftMot = BASE_SPEED - random(-10,10);
+  
+   //determining if the read values are large enought to trigger state transitions
+  //if both sensors on black then we need to turn around
+  if((rightLine < OB_THRESH_R) and (leftLine < OB_THRESH_L)) STATE = TURN_AROUND;
+
+  //case one is for right object detection
+  else if(rightLine < OB_THRESH_R) STATE = VEER_L;
+
+  //case two is for left obj detection
+  else if(leftLine < OB_THRESH_L) STATE = VEER_R;
+  
+  else STATE = WANDER;
+  
+  switch(STATE){
+
+    //case zero, the robot wanders
+    case WANDER:
+      motors(leftMot, rightMot);
+      break;
+    case TURN_AROUND:
+      spinLeft(BASE_SPEED);
+      delay(300);
+      motors(BASE_SPEED, BASE_SPEED);
+      break;
+    case VEER_L:
+      spinLeft(BASE_SPEED);
+      delay(100);
+      motors(BASE_SPEED, BASE_SPEED);
+      break;
+    case VEER_R:
+      spinRight(BASE_SPEED);
+      delay(100);
+      motors(BASE_SPEED, BASE_SPEED);
+      break;
+
+  
+  
+  
+  delay(100);        //wait .1 second
   
 } //closing curly of the “loop()” function
 
